583d4f7b290dd16727f2cc07a0226d00c4323bab
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-27 11:28:23 +0000

    Added start of react files, displaying title

---
+++ cookease_react/client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+// import LoginBox from '../auth/LoginBox'
+
+const Home = () => (
+  <div className="home">
+    <h1 className='title'>CookEase</h1>
+  </div>
+)
+
+    // <LoginBox url="http://localhost:5000/" />
+export default Home
