2475fb739b3910ab09e5acdf5b848348db47a3af
Lewis MacNee <lewis.macnee@hotmail.com>
2017-03-01 10:45:16 +0000

    Added ability to search for ingredient from list

--- cookease_react/client/src/app.js
+++ cookease_react/client/src/app.js
@@ -10,7 +10,16 @@ import Image from "./components/addRecipe/Image.jsx"
 
 import {Router, Route, IndexRoute, hashHistory} from "react-router"
 
-class App extends React.Component{
+export default class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      usedIngredients: [],
+      method: null,
+      image: null
+    }
+  }
 
   render(){
     return (
@@ -19,9 +28,9 @@ class App extends React.Component{
           <IndexRoute component={Home} />
           <Route path="Recipes" component={Recipes} />
           <Route path="AddRecipe" component={AddRecipe}>
-            <Route path="Ingredients" component={Ingredients}/>
-            <Route path="Method" component={Method}/>
-            <Route path="Image" component={Image}/>
+            <Route path="Ingredients" component={Ingredients} PassedDownThing={this.state}/>
+            <Route path="Method" component={Method} PassedDownThing={this.state}/>
+            <Route path="Image" component={Image} PassedDownThing={this.state}/>
           </Route>
         </Route>
       </Router>
--- cookease_react/client/src/components/AddRecipe.jsx
+++ cookease_react/client/src/components/AddRecipe.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import {Link} from "react-router"
 
-const AddRecipe = (props) => (
-  <div className="add-recipe">
-    <div id="add-recipe">
-      <div id="ingredients">
-      <h5>ingredients</h5>
-      </div>
-      <div id="recipe">
-      <h5>recipe</h5>
-      </div>
-      <div id="image">
-      <h5>image</h5>
-      </div>
-    </div>
-    <div id="user-input-box">
-    {props.children}
-    </div>
-  </div>
-)
+export default class AddRecipe extends React.Component{
 
-export default AddRecipe
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    
+    return(
+      <div className="add-recipe">
+        <div id="add-recipe">
+          <div id="ingredients">
+          <Link to="/addRecipe/ingredients">Ingredients</Link>
+          </div>
+          <div id="method">
+          <Link to="/addRecipe/method">Method</Link>
+          </div>
+          <div id="image">
+          <Link to="/addRecipe/image">Image</Link>
+          </div>
+        </div>
+        <div id="user-input-holder">
+        {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
